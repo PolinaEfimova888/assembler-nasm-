82b0b9b75e0030a8bf7eaa55b7ea5d40449f2a14
PolinaEfimova888 <57696453+PolinaEfimova888@users.noreply.github.com>
2021-01-28 02:29:12 +0800

    Update sqrt.asm

--- sqrt.asm
+++ sqrt.asm
@@ -26,43 +26,46 @@
 %macro dprint 1
     pushd
     mov ecx, 10
-    mov bx, 0 ; 2 Bytes MAX, Count Numbers
+    mov bx, 0 
     mov eax, %1
     
-%%_divide:
-    mov edx, 0
-    div ecx
-    push dx
-    inc bx
-    test eax, eax
-    jnz %%_divide
+    %%_divide:
+        mov edx, 0
+        div ecx
+        push dx
+        inc bx
+        test eax, eax
+        jnz %%_divide
     
-%%_digit:
-    pop ax
-    add ax, '0'
-    mov [symbol], ax
-    print 1, symbol
-    dec bx
-    cmp bx, 0
-    jg %%_digit
+    %%_digit:
+        pop ax
+        add ax, '0'
+        mov [count], ax
+        print 1, count
+        dec bx
+        cmp bx, 0
+        jg %%_digit
     
     popd
 %endmacro
 
-%macro calcX2 2
+%macro func 2
     push ebx
     push ecx
     push edx
     mov edx, 0
     mov ecx, %2
+    
     push ecx
     mov eax, %1
     div ecx
     pop edx
+    
     add eax, edx
     mov edx, 0
     mov ecx, 2
     div ecx
+    
     pop edx
     pop ecx
     pop ebx
@@ -75,44 +78,44 @@ global _start
 _start:
     mov eax, [num]
     mov ecx, 2
-    div ecx ; Result in EAX, friction part in EDX
+    div ecx 
     mov [a], eax
-    calcX2 [num], eax
-    mov[b], eax
+    func [num], eax
+    mov [b], eax
     
 _while:
     mov eax, [a]
     mov ecx, [b]
-    sub eax, ecx
-    cmp eax, 1
-    jl _end
+    sub eax, ecx 
+    cmp eax, 1 
+    jl _end  ; <
     
     mov eax, [b]
     mov [a], eax
-    calcX2 [num], [a]
-    mov[b], eax
+    func [num], [a]
+    mov [b], eax
     jmp _while
     
 _end:    
     dprint [b]
     
-    print nl, n
+    print nlen, n
     print len, message
-    print nl, n
+    print nlen, n
     
     mov     eax, 1
     int     0x80
 
 section.data
-    num DD 121
+    num DD 9
     
     message DB "Done"
     len EQU $ - message
     
     n DB 0xA, 0xD
-    nl EQU $ - n
+    nlen EQU $ - n
     
 section .bss
-    a resd 1 ; Reserve DWORD (1 Count)
+    count resd 1
+    a resd 1 
     b resd 1
-    symbol resb 1
